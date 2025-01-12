aa4a63e7eea38a9249b3bdb4b6e01c131512775f
Luch3r <luchkin_yrok@mail.ru>
2025-01-12 19:28:04 +0300

    yy

--- dz1/script.js
+++ dz1/script.js
@@ -1,16 +1,17 @@
-document.getElementById(`Button`).addEventListener(`click`,function() {
-    //const name = document.getElementById(`nameInput`).value;
-    const message = document.getElementById(`message`);
-
-    //if (name) {
-   //     message.textContent = `Привет, ${name}!`
-    //} else {
-   //     message.textContent = `Пожалуйста, введите свое имя!`;
-    //}
-
+document.getElementById('Button').addEventListener('click', function() {
+    // const name = prompt("Введите свое имя: ")
+    const name = document.getElementById('nameInput').value;
+    const message = document.getElementById('message');
 
     if (name) {
-        alert(`Привет, ${name}!`);
-        } else{
-            alert(`Пожалуйста, веедите свое имя!`);
-})
+        message.textContent = `Привет, ${name}!`;
+    } else {
+        message.textContent = 'Пожалуйста, введите свое имя!';
+    }
+
+    // if (name) {
+    //     alert(`Привет, ${name}!`);
+    // } else {
+    //     alert('Пожалуйста, введите свое имя!');
+    // }
+});
